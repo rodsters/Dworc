065c0df10ef720a0be887cfdf998bea972d560f2
rodsters <rodney.johnston01@yahoo.com>
2022-03-11 16:49:28 -0800

    Basic functionality for 2 sided turret
    Cringe that the upgrade path will be annoying to fix. Probably have to restructure crap ton of code :)))))

--- Assets/GoldScript.cs
+++ Assets/GoldScript.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class GoldScript : MonoBehaviour
 {
-
+    // public 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Bullet")
         {
             //add gold count
-            GameObject.Find("speckyTower").GetComponent<TowerManager>().gold++;
+            //GameObject.Find("speckyTower").GetComponent<TowerManager>().gold++;
+            GameObject.Find("speckyTower 2").GetComponent<TowerManager>().gold++;
             Destroy(gameObject);
         }
     }
